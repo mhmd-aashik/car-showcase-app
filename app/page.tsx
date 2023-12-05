dc4ff9741c8b241da4b424f21b9a587208e06f5e
aashik <aashikdevelop@gmail.com>
2023-12-06 02:52:32 +0530

    Fetch Api Typescript & Car Card

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import CarCard from "@/components/CarCard";
 import CustomFilter from "@/components/CustomFilter";
 import Hero from "@/components/Hero";
 import SearchBar from "@/components/SearchBar";
@@ -6,6 +7,8 @@ import { fetchCars } from "@/utils";
 export default async function Home() {
   const allCars = await fetchCars();
   console.log(allCars);
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
   return (
     <main className="overflow-hidden">
@@ -24,6 +27,23 @@ export default async function Home() {
             <CustomFilter title="year" />
           </div>
         </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-red-500 text-xl font-bold">
+              oops, no results😒!
+            </h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -8,6 +8,9 @@ interface CustomButtonProps {
   containerStyles: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
   btnType?: "button" | "submit";
+  textStyles?: string;
+  rightIcon?: string;
+  isDisabled?: boolean;
 }
 
 const CustomButton = ({
@@ -15,15 +18,28 @@ const CustomButton = ({
   containerStyles,
   handleClick,
   btnType,
+  textStyles,
+  rightIcon,
+  isDisabled,
 }: CustomButtonProps) => {
   return (
     <button
       disabled={false}
       type={btnType || "button"}
-      className={`custom-btn ${containerStyles}`}
+      className={`custom-btn ${containerStyles} `}
       onClick={handleClick}
     >
-      <span className={`flex-1`}>{title}</span>
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative h-6 w-6">
+          <Image
+            src={rightIcon}
+            alt="right icon"
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
     </button>
   );
 };
